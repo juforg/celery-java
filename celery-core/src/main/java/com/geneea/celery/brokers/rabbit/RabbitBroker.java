4a57354411db92b22955251148bcf8a12ecc06cb
Tomáš Mudruňka <mudrunka@geneea.com>
2018-04-10 16:24:51 +0200

    CELERY-11 use method reference instead of anonymous classes, also handle UncheckedExecutionException

--- celery-core/src/main/java/com/geneea/celery/brokers/rabbit/RabbitBroker.java
+++ celery-core/src/main/java/com/geneea/celery/brokers/rabbit/RabbitBroker.java
@@ -6,13 +6,14 @@ import com.geneea.celery.spi.Message;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.cache.RemovalListener;
 import com.google.common.cache.RemovalNotification;
+import com.google.common.util.concurrent.UncheckedExecutionException;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -22,30 +23,38 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class RabbitBroker implements Broker {
 
-    final Connection connection;
+    private final Connection connection;
     private final LoadingCache<Long, Channel> channels = CacheBuilder.newBuilder()
             .expireAfterAccess(10, TimeUnit.MINUTES)
-            .removalListener(new RemovalListener<Long, Channel>() {
-                @Override
-                public void onRemoval(RemovalNotification<Long, Channel> notification) {
-                    if (!connection.isOpen()) {
-                        return;
-                    }
-                    try {
-                        notification.getValue().abort();
-                    } catch (IOException e) {
-                        log.info("Error when closing channel.", e);
-                    }
-                }
-            }).build(new CacheLoader<Long, Channel>() {
-                @Override
-                public Channel load(Long key) throws Exception {
-                    return connection.createChannel();
-                }
-            });
+            .removalListener(this::closeRemovedChannel)
+            .build(CacheLoader.from(this::createChannel));
 
     public RabbitBroker(Connection connection) {
         this.connection = connection;
+    }
+
+    private Channel createChannel() {
+        try {
+            return connection.createChannel();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private void closeRemovedChannel(RemovalNotification<Long, Channel> notification) {
+        if (!connection.isOpen()) {
+            return;
+        }
+        try {
+            Channel channel = notification.getValue();
+            if (channel != null) {
+                channel.abort();
+            } else {
+                log.warn("RemovalNotification without channel, cause={}", notification.getCause());
+            }
+        } catch (IOException e) {
+            log.warn("Error when closing channel.", e);
+        }
     }
 
     @Override
@@ -60,10 +69,14 @@ public class RabbitBroker implements Broker {
     Channel getChannel() throws IOException {
         try {
             return channels.get(Thread.currentThread().getId());
-        } catch (ExecutionException e) {
+        } catch (ExecutionException | UncheckedExecutionException e) {
             final Throwable cause = e.getCause();
             if (cause instanceof IOException) {
                 throw (IOException) cause;
+            } else if (cause instanceof UncheckedIOException) {
+                throw ((UncheckedIOException) cause).getCause();
+            } else if (cause instanceof RuntimeException) {
+                throw (RuntimeException) cause;
             } else {
                 throw new RuntimeException(cause);
             }
